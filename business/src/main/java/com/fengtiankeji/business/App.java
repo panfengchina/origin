f5186b4e65b4b841d00f5c6f9b945eba276ef3b7
feng.pan <feng.pan@mobilemd.cn>
2017-10-29 18:11:42 +0800

    maven骨架

---
+++ business/src/main/java/com/fengtiankeji/business/App.java
@@ -0,0 +1,13 @@
+package com.fengtiankeji.business;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
